8a0c1e012f5376431b8855461c4c8991d5d28de3
an55664 <33439372+an55664@users.noreply.github.com>
2024-01-24 14:54:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // JavaScript 示例（使用 web3.js）
 
 // 检查是否具有 Web3 提供程序
-if (typeof Web3 !== 'undefined') {
+/*if (typeof Web3 !== 'undefined') {
     // Web3 已加载
 } else {
     console.error("Web3 not detected. Please check if it's included.");
-}
+}*/
 if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     try {
